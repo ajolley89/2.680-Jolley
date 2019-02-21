2f57a79526dd007c7996256fdf30103985803c0f
ajolley89 <ajolley89@gmail.com>
2019-02-21 09:57:20 -0500

    Initial upload

---
+++ src/pPrimeFactor/PrimeFactor.cpp
@@ -0,0 +1,129 @@
+/************************************************************/
+/*    NAME: Austin Jolley                                              */
+/*    ORGN: MIT                                             */
+/*    FILE: PrimeFactor.cpp                                        */
+/*    DATE:                                                 */
+/************************************************************/
+
+#include <iterator>
+#include "MBUtils.h"
+#include "PrimeFactor.h"
+
+using namespace std;
+
+//---------------------------------------------------------
+// Constructor
+
+PrimeFactor::PrimeFactor()
+{
+  m_New_Number = 0;
+  m_number_result = "";
+
+}
+
+//---------------------------------------------------------
+// Destructor
+
+PrimeFactor::~PrimeFactor()
+{
+
+}
+
+//---------------------------------------------------------
+// Procedure: OnNewMail
+
+bool PrimeFactor::OnNewMail(MOOSMSG_LIST &NewMail)
+{
+  MOOSMSG_LIST::iterator p;
+   
+  for(p=NewMail.begin(); p!=NewMail.end(); p++) {
+    CMOOSMsg &msg = *p;
+
+    string key = msg.GetKey();
+    if(key=="NUM_VALUE") {
+      m_New_Number = msg.GetDouble();
+    }
+
+    if (m_New_Number % 2 == 0) {
+      m_number_result = to_string(m_New_Number) + ", is even.";
+    }
+    else {
+      m_number_result = to_string(m_New_Number) + ", is odd." ;
+    }
+
+    Notify("NUM_RESULT", m_number_result);
+
+#if 0 // Keep these around just for template
+    string key   = msg.GetKey();
+    string comm  = msg.GetCommunity();
+    double dval  = msg.GetDouble();
+    string sval  = msg.GetString(); 
+    string msrc  = msg.GetSource();
+    double mtime = msg.GetTime();
+    bool   mdbl  = msg.IsDouble();
+    bool   mstr  = msg.IsString();
+#endif
+   }
+	
+   return(true);
+}
+
+//---------------------------------------------------------
+// Procedure: OnConnectToServer
+
+bool PrimeFactor::OnConnectToServer()
+{
+   // register for variables here
+   // possibly look at the mission file?
+   // m_MissionReader.GetConfigurationParam("Name", <string>);
+   // m_Comms.Register("VARNAME", 0);
+	
+   RegisterVariables();
+   return(true);
+}
+
+//---------------------------------------------------------
+// Procedure: Iterate()
+//            happens AppTick times per second
+
+bool PrimeFactor::Iterate()
+{
+  return(true);
+}
+
+//---------------------------------------------------------
+// Procedure: OnStartUp()
+//            happens before connection is open
+
+bool PrimeFactor::OnStartUp()
+{
+  list<string> sParams;
+  m_MissionReader.EnableVerbatimQuoting(false);
+  if(m_MissionReader.GetConfiguration(GetAppName(), sParams)) {
+    list<string>::iterator p;
+    for(p=sParams.begin(); p!=sParams.end(); p++) {
+      string line  = *p;
+      string param = tolower(biteStringX(line, '='));
+      string value = line;
+      
+      if(param == "foo") {
+        //handled
+      }
+      else if(param == "bar") {
+        //handled
+      }
+    }
+  }
+  
+  RegisterVariables();	
+  return(true);
+}
+
+//---------------------------------------------------------
+// Procedure: RegisterVariables
+
+void PrimeFactor::RegisterVariables()
+{
+   Register("NUM_VALUE", 0);
+}
+
---
+++ src/pPrimeFactor/PrimeFactor.h
@@ -0,0 +1,37 @@
+/************************************************************/
+/*    NAME: Austin Jolley                                              */
+/*    ORGN: MIT                                             */
+/*    FILE: PrimeFactor.h                                          */
+/*    DATE:                                                 */
+/************************************************************/
+
+#ifndef PrimeFactor_HEADER
+#define PrimeFactor_HEADER
+
+#include <string>
+#include "MOOS/libMOOS/MOOSLib.h"
+using namespace std;
+
+class PrimeFactor : public CMOOSApp
+{
+ public:
+   PrimeFactor();
+   ~PrimeFactor();
+
+ protected: // Standard MOOSApp functions to overload  
+   bool OnNewMail(MOOSMSG_LIST &NewMail);
+   bool Iterate();
+   bool OnConnectToServer();
+   bool OnStartUp();
+
+ protected:
+   void RegisterVariables();
+   int m_New_Number;
+   string m_number_result;
+
+ private: // Configuration variables
+
+ private: // State variables
+};
+
+#endif 
